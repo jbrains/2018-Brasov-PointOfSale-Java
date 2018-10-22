eebb13b2b968dc7c6dd90dd1e51161d02c65c4ec
J. B. Rainsberger <me@jbrains.ca>
2018-10-22 15:16:41 +0300

    We are ready to sell items of any price.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -1,28 +1,52 @@
 package ca.jbrains.pos.test;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SellOneItemTest {
     @Test
     public void productFound() throws Exception {
         Display display = new Display();
-        Sale sale = new Sale();
+        Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assert.assertEquals("RON 6.50", display.getText());
     }
 
+    @Test
+    @Ignore("refactoring")
+    public void anotherProductFound() throws Exception {
+        Display display = new Display();
+        Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assert.assertEquals("RON 12.75", display.getText());
+    }
+
     public static class Display {
+        private String text;
+
         public String getText() {
-            return "RON 6.50";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 
     public static class Sale {
-        public void onBarcode(String barcode) {
+        private Display display;
 
+        public Sale(Display display) {
+            this.display = display;
+        }
+
+        public void onBarcode(String barcode) {
+            display.setText("RON 6.50");
         }
     }
 }
