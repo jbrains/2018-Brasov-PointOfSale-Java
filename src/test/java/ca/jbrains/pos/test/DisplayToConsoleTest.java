5984da671faeec4d64810f2a8d6cd1bb993d6bf6
J. B. Rainsberger <me@jbrains.ca>
2018-10-23 20:31:30 +0300

    We can now 'display' a price, as long as it's EUR 7.95.

--- src/test/java/ca/jbrains/pos/test/DisplayToConsoleTest.java
+++ src/test/java/ca/jbrains/pos/test/DisplayToConsoleTest.java
@@ -21,6 +21,17 @@ public class DisplayToConsoleTest {
         Assert.assertEquals(List.of("Product not found: ::missing barcode::"), lines(canvas.toString()));
     }
 
+    @Test
+    public void price() throws Exception {
+        StringWriter canvas = new StringWriter();
+        displayPrice(canvas, Price.euroCents(795));
+        Assert.assertEquals(List.of("EUR 7.95"), lines(canvas.toString()));
+    }
+
+    private void displayPrice(StringWriter canvas, Price price) {
+        new PrintWriter(canvas, true).printf("EUR %.2f", price.inEuro()).println();
+    }
+
     private void displayProductNotFoundMessage(Writer canvas, String missingBarcode) {
         new PrintWriter(canvas, true).printf("Product not found: %s", missingBarcode).println();
     }
--- src/test/java/ca/jbrains/pos/test/Price.java
+++ src/test/java/ca/jbrains/pos/test/Price.java
@@ -1,12 +1,22 @@
 package ca.jbrains.pos.test;
 
 public class Price {
+    private final int euroCents;
+
+    public Price(int euroCents) {
+        this.euroCents = euroCents;
+    }
+
     public static Price euroCents(int euroCents) {
-        return new Price();
+        return new Price(euroCents);
     }
 
     @Override
     public String toString() {
         return "a Price";
+    }
+
+    public double inEuro() {
+        return (double) (euroCents / 100.0d);
     }
 }
